10403cd62183e8997f8a1e55b94b08c4595d3779
Julien Schwartzmann <julienschwartzmann@192.168.1.244>
2023-05-08 18:01:05 +0100

    another commit

--- Recommendation_system.py
+++ Recommendation_system.py
@@ -17,7 +17,7 @@ if user_food_pref not in types:
     suggestions_list = autocomplete(user_food_pref,types)
     while suggestions_list:
         print("Oops, it looks like we do not have such cuisine around, did you mean: {0}".format(", ".join(suggestions_list)))
-        user_food_pref= input("?")
+        user_food_pref= input("?\n")
         if user_food_pref in types:
             break
         else:
