c2f794780fb8cc2b360123f3618042b7c60f4ff4
Julien Schwartzmann <julienschwartzmann@192.168.1.244>
2023-05-08 17:17:11 +0100

    another commit

--- Recommendation_system.py
+++ Recommendation_system.py
@@ -3,5 +3,11 @@ from restaurantData import types, restaurant_data
 # for item in types:
 #     print(item)
 
+user_food_pref = "c"
+lg = len(user_food_pref)
+
+possible_food_choices = [x for x in types if user_food_pref in x[:n]]
+
+print(possible_food_choices)
 
 
