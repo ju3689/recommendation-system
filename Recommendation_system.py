0957dbe46b5dc151d990922094ecc27c63f99e48
Julien Schwartzmann <julienschwartzmann@192.168.1.244>
2023-05-08 17:48:35 +0100

    another commit

--- Recommendation_system.py
+++ Recommendation_system.py
@@ -3,11 +3,23 @@ from restaurantData import types, restaurant_data
 # for item in types:
 #     print(item)
 
+
+def autocomplete(food_pref, list_options):
+
+    lg = len(food_pref)
+    possible_food_choices = [x for x in list_options if food_pref in x[:lg]]
+    return possible_food_choices
+
+
 user_food_pref = "aus"
-lg = len(user_food_pref)
 
-possible_food_choices = [x for x in types if user_food_pref in x[:lg]]
+if user_food_pref not in types:
+    suggestions_list = autocomplete(user_food_pref,types)
+    while suggestions_list:
+        print("Oops, it looks like we do not have such cuisine around, did you mean:\n")
+        print(*suggestions_list, sep=", ")
+        print("?")
+        user_food_pref = input("\n")
+        suggestions_list = autocomplete(user_food_pref,types)
 
-print(possible_food_choices)
-
-
+valid_pref = user_food_pref
