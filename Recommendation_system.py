f776e4a5a1891f56143d67c123624a8622e290f6
Julien Schwartzmann <julienschwartzmann@192.168.1.244>
2023-05-08 17:54:05 +0100

    another commit

--- Recommendation_system.py
+++ Recommendation_system.py
@@ -16,7 +16,7 @@ user_food_pref = "aus"
 if user_food_pref not in types:
     suggestions_list = autocomplete(user_food_pref,types)
     while suggestions_list:
-        user_food_pref= input("Oops, it looks like we do not have such cuisine around, did you mean:", ''.join(suggestions_list), " ?")
+        user_food_pref= input("Oops, it looks like we do not have such cuisine around, did you mean: {0}".format("".join(suggestions_list)))
         if user_food_pref in types:
             break
         else:
