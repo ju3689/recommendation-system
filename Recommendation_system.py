8765ac8db61eec334715e34cabfead0d5b89f095
Julien Schwartzmann <julienschwartzmann@192.168.1.244>
2023-05-10 17:10:33 +0100

    another c

--- Recommendation_system.py
+++ Recommendation_system.py
@@ -13,30 +13,25 @@ def autocomplete_list(food_pref, list_options):
 
 def handle_input_and_choices(food_pref, list_options):
     suggestions_list = autocomplete_list(food_pref, list_options)
-    valid_choice = False
-    if food_pref in list_options: valid_choice = True
-    
-    if valid_choice:
-        if len(suggestions_list) > 1:
-            food_pref=input("Nice one! We have a few options for that ..! Which one specifically do you fancy ? : {0} \n", ", ".join(suggestions_list))
-            return handle_input_and_choices(food_pref, list_options)
-    else:
-        if len(suggestions_list) == 1:
-            user_confirm_choice =input("One more effort, did you mean: {0} ? Type \"Yes\" or \"No\" \n".format(", ".join(suggestions_list)))
-            if user_confirm_choice == "Yes":
-                 food_pref = suggestions_list[0]
-            elif user_confirm_choice == "No":
-                food_pref = input("Ok let's try again. What type of food do you fancy eating ?\n")
-                return handle_input_and_choices(food_pref, list_options)
-            else:
-                food_pref = input("Hmm does not seem like a valid input, let's try again. What type of food do you fancy eating ?\n")
-                return handle_input_and_choices(food_pref, list_options)
-        elif len(suggestions_list) > 1:
-            food_pref=input("We have a few options for that ..! Which one specifically do you fancy ? : {0} \n".format(", ".join(suggestions_list)))
+    # valid_choice = False
+    # if food_pref in list_options: valid_choice = True
+
+    if len(suggestions_list) == 1:
+        user_confirm_choice =input("One more effort, did you mean: {0} ? Type \"Yes\" or \"No\" \n".format(", ".join(suggestions_list)))
+        if user_confirm_choice == "Yes":
+            food_pref = suggestions_list[0]
+        elif user_confirm_choice == "No":
+            food_pref = input("Ok let's try again. What type of food do you fancy eating ?\n")
             return handle_input_and_choices(food_pref, list_options)
         else:
-            food_pref = input("Tough one ... we do not have such type of cuisine around, try something else.\n")
-            return handle_input_and_choices(food_pref, list_options) 
+            food_pref = input("Hmm does not seem like a valid input, let's try again. What type of food do you fancy eating ?\n")
+            return handle_input_and_choices(food_pref, list_options)
+    elif len(suggestions_list) > 1:
+        food_pref=input("We have a few options for that ..! Which one specifically do you fancy ? : {0} \n".format(", ".join(suggestions_list)))
+        return handle_input_and_choices(food_pref, list_options)
+    else:
+        food_pref = input("Tough one ... we do not have such type of cuisine around, try something else.\n")
+        return handle_input_and_choices(food_pref, list_options) 
 
     return food_pref
 
